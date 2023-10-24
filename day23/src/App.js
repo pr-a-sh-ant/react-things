4f19a9ebebc83f90f969c977a525479b41989359
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-10-24 20:40:46 +0545

    Learning about react-transistion-group

--- day23/src/App.js
+++ day23/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Transition } from "react-transition-group";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,19 +7,36 @@ import List from "./components/List/List";
 
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [showblock, setshowblock] = useState(false);
 
   const onClickHandler = () => {
-    setModalIsOpen(true);
-  };
-
-  const onCloseHandler = () => {
-    setModalIsOpen(false);
+    setModalIsOpen(!modalIsOpen);
   };
 
   return (
     <div className="App">
       <h1>React Animations</h1>
-      <Modal show={modalIsOpen} closed={onCloseHandler} />
+      <button
+        className="Button"
+        onClick={() => {
+          setshowblock(!showblock);
+        }}
+      >
+        toggle
+      </button>
+      <br />
+      <Transition>
+        <div
+          style={{
+            margin: "auto",
+            backgroundColor: "red",
+            width: 100,
+            height: 100,
+          }}
+        ></div>
+      </Transition>
+
+      <Modal show={modalIsOpen} closed={onClickHandler} />
       <Backdrop show={modalIsOpen} />
       <button className="Button" onClick={onClickHandler}>
         Open Modal
