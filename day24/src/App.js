c82ad6039607ddbbb99d0c393978e2c3e92c739c
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-10-26 21:48:46 +0545

    learning test

---
+++ day24/src/App.js
@@ -0,0 +1,12 @@
+import Greeting from "./components/greeting";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ day24/src/components/greeting.test.js
@@ -0,0 +1,12 @@
+import Greeting from "./greeting";
+import { render, screen } from "@testing-library/react";
+
+test("Renders Hello World as a text", () => {
+  //Arrange
+  render(<Greeting />);
+
+  //Act
+  //...nothing
+
+  //Assert
+});
